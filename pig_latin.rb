94caf112a8bde998885658c48b921094600d3047
klorealf & Samory Harris <pair+klorealf.samory070@devbootcamp.com>
2017-08-29 19:26:53 -0400

    Make rspec and pig_latin.rb

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,1 +1,29 @@
 # Your code here
+# IF the word starts with a vowel, return the word.
+# ELSE return the word's pig latin equivalent.
+#   MOVE all leading consonants to the end of the word
+#   and add the suffix "ay."
+# ENDIF
+
+def single_word(word)
+  word = word.split('')
+  vowels = ["a","e","i","o","u"]
+  count = 0
+  word.each do |char|
+    vowels.each do |vowel|
+      if char== vowel
+        count +=1
+        break
+      end
+    end
+  end
+else
+    word<<word.shift
+    word.join + "ay"
+  end
+end
+
+def sentence
+
+end
+
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -1,7 +1,24 @@
 require_relative('../pig_latin')
 
-describe 'Converting single words to Pig Latin' do
+# CONVERT SINGLE WORD
+
+
+
+describe 'Converts vowel to Pig Latin' do
+
+it 'it will convert vowel to pig latin' do
+expect(single_word("elephant")).to eq ("elephant")
 end
 
+it 'it will convert consonant to pig latin' do
+expect(single_word("car")).to eq ("arcay")
+end
+end
+
+
+
 describe 'Converting a sentence to Pig Latin' do
+  it 'it will convert a sentence to Pig latin' do
+expect(sentence("The elephant is big")).to eq ("Hetay elephant is igbay")
+end
 end
