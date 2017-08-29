9f3851ca7242019b72484197794481d9031de2b8
Catherine Rice & Chris Camp <pair+clr2107.cpcamp2@devbootcamp.com>
2017-08-29 17:09:33 -0400

    Initial commit

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,1 +1,11 @@
-# Your code here
+# CONVERT SINGLE WORD
+
+# IF the word starts with a vowel, return the word.
+# ELSE return the word's pig latin equivalent.
+#   MOVE all leading consonants to the end of the word
+#   and add the suffix "ay."
+# ENDIF
+
+def convert_word_to_pig_latin(word)
+
+end#convert_word_to_pig_latin
